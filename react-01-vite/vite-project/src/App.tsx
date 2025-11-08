05defba38a9a2b1db415d4002ded0783a60309fe
lkarzhalovi <karzhalovi@gmail.com>
2025-11-08 23:26:17 +0400

    <feat>(react) create first react projects (vite,  create-react-app)

---
+++ react-01-vite/vite-project/src/App.tsx
@@ -0,0 +1,11 @@
+import Text from "./Components/Text"
+
+function App() {
+  return (
+    <>
+    <Text />
+    </>
+  )
+}
+
+export default App
---
+++ react-01-vite/vite-project/src/Components/Text.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+const Text = () : React.JSX.Element => {
+return (
+    <h1>
+    gilocav gilvcav
+    </h1>
+)
+}
+
+export default Text;
